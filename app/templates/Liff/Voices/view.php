f0143404a5fed36ee2f5bad607e60974a84823a4
WatashiHaNeko <dev@neko.report>
2020-05-07 20:55:10 +0900

    通知一覧UI実装

--- app/templates/Liff/Voices/view.php
+++ app/templates/Liff/Voices/view.php
@@ -3,6 +3,7 @@
     'justify-content' => 'space-between',
     'padding' => '32px 16px',
     'background-color' => '#ffffff',
+    'border-bottom' => 'solid 1px #d1d1d6',
   ]) ?>">
   <div>
     <div style="<?= $this->Html->style([
@@ -27,6 +28,74 @@
       <?= __('編集') ?>
     </a>
   </div>
+</div>
+
+<div style="<?= $this->Html->style([
+    'padding-bottom' => '108px',
+  ]) ?>">
+  <?php foreach ($voice['message_schedules'] as $messageSchedule): ?>
+  <div style="<?= $this->Html->style([
+      'display' => 'flex',
+      'justify-content' => 'flex-start',
+      'align-items' => 'stretch',
+      'padding' => '8px 16px',
+      'background-color' => '#ffffff',
+      'border-bottom' => 'solid 1px #d1d1d6',
+    ]) ?>">
+    <div>
+      <p style="<?= $this->Html->style([
+          'box-sizing' => 'border-box',
+          'margin' => '0',
+          'padding-top' => '16px',
+          'width' => '64px',
+          'height' => '64px',
+          'background-color' => '#007aff',
+          'border-radius' => '32px',
+          'color' => '#ffffff',
+          'font-weight' => '800',
+        ]) ?>">
+        <span style="<?= $this->Html->style([
+            'display' => 'block',
+            'line-height' => '12px',
+            'font-size' => '12px',
+            'text-align' => 'center',
+          ]) ?>">
+          <?= $messageSchedule['scheduled_time']->i18nFormat('a', 'Asia/Tokyo') ?>
+        </span>
+
+        <span style="<?= $this->Html->style([
+            'display' => 'block',
+            'line-height' => '16px',
+            'font-size' => '16px',
+            'text-align' => 'center',
+          ]) ?>">
+          <?= $messageSchedule['scheduled_time']->i18nFormat('h:mm', 'Asia/Tokyo') ?>
+        </span>
+      </p>
+    </div>
+
+    <div style="<?= $this->Html->style([
+        'margin-left' => '16px',
+      ]) ?>">
+      <p style="<?= $this->Html->style([
+          'margin' => '0',
+        ]) ?>">
+        <?= nl2br(h($messageSchedule['message'])) ?>
+      </p>
+    </div>
+
+    <div style="<?= $this->Html->style([
+        'display' => 'flex',
+        'align-items' => 'center',
+        'margin-left' => 'auto',
+      ]) ?>">
+      <i class="fas fa-chevron-right" style="<?= $this->Html->style([
+          'color' => '#8e8e93',
+          'font-size' => '16px',
+        ]) ?>"></i>
+    </div>
+  </div>
+  <?php endforeach; ?>
 </div>
 
 <?php if (empty($voice['message_schedules'])): ?>
