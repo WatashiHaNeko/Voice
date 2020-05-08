4db08f25ad675dc1a0bb3a3b59f3d879d39e6066
WatashiHaNeko <dev@neko.report>
2020-05-08 22:37:24 +0900

    ランディングページ追加

--- app/templates/Pages/home.php
+++ app/templates/Pages/home.php
@@ -1,207 +1,141 @@
 <?php
-use Cake\Cache\Cache;
 use Cake\Core\Configure;
-use Cake\Core\Plugin;
-use Cake\Datasource\ConnectionManager;
-use Cake\Error\Debugger;
-use Cake\Http\Exception\NotFoundException;
 
 $this->disableAutoLayout();
-
-if (!Configure::read('debug')) {
-  throw new NotFoundException('Please replace templates/Pages/home.php with your own version or re-enable debug mode.');
-}
-
-$cakeDescription = 'CakePHP: the rapid development PHP framework';
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-  <?= $this->Html->charset() ?>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>
-    <?= $cakeDescription ?>:
-    <?= $this->fetch('title') ?>
-  </title>
-  <?= $this->Html->meta('icon') ?>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="robots" content="noindex">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 
-  <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.1/normalize.css">
+    <meta property="og:url" content="<?= $this->Url->build([], ['fullBase' => true]) ?>">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="<?= __('わんわんボイス') ?>">
+    <meta property="og:image" content="<?= $this->Url->image('ogp.jpg', ['fullBase' => true]) ?>">
+    <meta property="og:description" content="<?= __('指定した時間にペットからLINEでメッセージが届く！') ?>">
+    <meta property="og:site_name" content="<?= __('わんわんボイス') ?>">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:creator" content="@watashi_ha_neko">
 
-  <?= $this->Html->css('milligram.min.css') ?>
-  <?= $this->Html->css('cake.css') ?>
-  <?= $this->Html->css('home.css') ?>
+    <?= $this->Html->css('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@400;800&display=swap') ?>
 
-  <?= $this->fetch('meta') ?>
-  <?= $this->fetch('css') ?>
-  <?= $this->fetch('script') ?>
-</head>
-<body>
-  <header>
-    <div class="container text-center">
-      <a href="https://cakephp.org/" target="_blank">
-        <img alt="CakePHP" src="https://cakephp.org/v2/img/logos/CakePHP_Logo.svg" width="350" />
+    <style>
+    .button {
+      position: relative;
+      top: -4px;
+      display: block;
+      box-sizing: border-box;
+      padding: 12px 24px;
+      width: 100%;
+      line-height: 20px;
+      background-color: #34c759;
+      background: linear-gradient(0, #34c759 0%, #30d158 50%);
+      border: none;
+      border-radius: 21px;
+      box-shadow: 0 4px 8px 0 #8e8e93;
+      color: #ffffff;
+      font-size: 20px;
+      font-weight: 800;
+      text-align: center;
+      text-decoration: none;
+      transition-duration: .2s;
+    }
+
+    .button:hover {
+      outline: none;
+    }
+
+    .button:hover {
+      top: -8px;
+      box-shadow: 0 8px 8px 0 #8e8e93;
+    }
+
+    .button:active {
+      top: 0;
+      box-shadow: 0 0 8px 0 #8e8e93;
+    }
+    </style>
+
+    <title>
+      <?= sprintf('%s | %s', __('わんわんボイス'), __('指定した時間にペットからLINEでメッセージが届く！')) ?>
+    </title>
+  </head>
+
+  <body style="<?= $this->Html->style([
+      'box-sizing' => 'border-box',
+      'margin' => '0',
+      'padding' => '16px',
+      'font-family' => '\'M PLUS Rounded 1c\', sans-serif',
+      'border' => 'solid 8px #34c759',
+      'min-height' => '100vh',
+    ]) ?>">
+    <div style="<?= $this->Html->style([
+        'margin' => '48px auto 0',
+        'width' => '160px',
+        'height' => '160px',
+      ]) ?>">
+      <img src="<?= $this->Url->image('avatar-default.jpg') ?>" style="<?= $this->Html->style([
+          'display' => 'block',
+          'box-sizing' => 'border-box',
+          'padding' => '4px',
+          'width' => '100%',
+          'height' => '100%',
+          'background-color' => '#ffffff',
+          'border' => 'solid 2px #d1d1d6',
+          'border-radius' => '50%',
+          'object-fit' => 'cover',
+        ]) ?>">
+    </div>
+
+    <h1 style="<?= $this->Html->style([
+        'margin' => '24px 0 0',
+        'color' => '#34c759',
+        'font-size' => '40px',
+        'font-weight' => '800',
+        'text-align' => 'center',
+      ]) ?>">
+      <?= __('わんわんボイス') ?>
+    </h1>
+
+    <p style="<?= $this->Html->style([
+        'margin' => '24px 0 0',
+        'color' => '#34c759',
+        'font-size' => '24px',
+        'font-weight' => '400',
+        'text-align' => 'center',
+      ]) ?>">
+      <?= implode('<br>', [
+        __('指定した時間に'),
+        __('ペットから'),
+        __('LINEでメッセージが届く！'),
+      ]) ?>
+    </p>
+
+    <div style="<?= $this->Html->style([
+        'margin' => '36px 0 0 ',
+      ]) ?>">
+      <a href="<?= vsprintf('%s://%s/%s', [
+        'https',
+        implode('.', ['line', 'me']),
+        implode('/', ['R', 'ti', 'p', Configure::read('Line.OfficialAccount.id')]),
+      ]) ?>" class="button">
+        <?= __('まずはLINEで友だち登録！') ?>
       </a>
-      <h1>
-        Welcome to CakePHP <?php echo Configure::version() ?> Strawberry
-      </h1>
     </div>
-  </header>
-  <main class="main">
-    <div class="container">
-      <div class="content">
-        <div class="row">
-          <div class="column">
-            <div class="message default text-center">
-              <small>Please be aware that this page will not be shown if you turn off debug mode unless you replace templates/Pages/home.php with your own version.</small>
-            </div>
-            <!-- <div id="url-rewriting-warning" class="alert url-rewriting">
-              <ul>
-                <li class="bullet problem">
-                  URL rewriting is not properly configured on your server.<br />
-                  1) <a target="_blank" href="https://book.cakephp.org/4/en/installation.html#url-rewriting">Help me configure it</a><br />
-                  2) <a target="_blank" href="https://book.cakephp.org/4/en/development/configuration.html#general-configuration">I don't / can't use URL rewriting</a>
-                </li>
-              </ul>
-            </div> -->
-            <?php Debugger::checkSecurityKeys(); ?>
-          </div>
-        </div>
-        <div class="row">
-          <div class="column">
-            <h4>Environment</h4>
-            <ul>
-            <?php if (version_compare(PHP_VERSION, '7.2.0', '>=')) : ?>
-              <li class="bullet success">Your version of PHP is 7.2.0 or higher (detected <?php echo PHP_VERSION ?>).</li>
-            <?php else : ?>
-              <li class="bullet problem">Your version of PHP is too low. You need PHP 7.2.0 or higher to use CakePHP (detected <?php echo PHP_VERSION ?>).</li>
-            <?php endif; ?>
 
-            <?php if (extension_loaded('mbstring')) : ?>
-              <li class="bullet success">Your version of PHP has the mbstring extension loaded.</li>
-            <?php else : ?>
-              <li class="bullet problem">Your version of PHP does NOT have the mbstring extension loaded.</li>
-            <?php endif; ?>
-
-            <?php if (extension_loaded('openssl')) : ?>
-              <li class="bullet success">Your version of PHP has the openssl extension loaded.</li>
-            <?php elseif (extension_loaded('mcrypt')) : ?>
-              <li class="bullet success">Your version of PHP has the mcrypt extension loaded.</li>
-            <?php else : ?>
-              <li class="bullet problem">Your version of PHP does NOT have the openssl or mcrypt extension loaded.</li>
-            <?php endif; ?>
-
-            <?php if (extension_loaded('intl')) : ?>
-              <li class="bullet success">Your version of PHP has the intl extension loaded.</li>
-            <?php else : ?>
-              <li class="bullet problem">Your version of PHP does NOT have the intl extension loaded.</li>
-            <?php endif; ?>
-            </ul>
-          </div>
-          <div class="column">
-            <h4>Filesystem</h4>
-            <ul>
-            <?php if (is_writable(TMP)) : ?>
-              <li class="bullet success">Your tmp directory is writable.</li>
-            <?php else : ?>
-              <li class="bullet problem">Your tmp directory is NOT writable.</li>
-            <?php endif; ?>
-
-            <?php if (is_writable(LOGS)) : ?>
-              <li class="bullet success">Your logs directory is writable.</li>
-            <?php else : ?>
-              <li class="bullet problem">Your logs directory is NOT writable.</li>
-            <?php endif; ?>
-
-            <?php $settings = Cache::getConfig('_cake_core_'); ?>
-            <?php if (!empty($settings)) : ?>
-              <li class="bullet success">The <em><?php echo $settings['className'] ?>Engine</em> is being used for core caching. To change the config edit config/app.php</li>
-            <?php else : ?>
-              <li class="bullet problem">Your cache is NOT working. Please check the settings in config/app.php</li>
-            <?php endif; ?>
-            </ul>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="column">
-            <h4>Database</h4>
-            <?php
-            try {
-              $connection = ConnectionManager::get('default');
-              $connected = $connection->connect();
-            } catch (Exception $connectionError) {
-              $connected = false;
-              $errorMsg = $connectionError->getMessage();
-              if (method_exists($connectionError, 'getAttributes')) :
-                $attributes = $connectionError->getAttributes();
-                if (isset($errorMsg['message'])) :
-                  $errorMsg .= '<br />' . $attributes['message'];
-                endif;
-              endif;
-            }
-            ?>
-            <ul>
-            <?php if ($connected) : ?>
-              <li class="bullet success">CakePHP is able to connect to the database.</li>
-            <?php else : ?>
-              <li class="bullet problem">CakePHP is NOT able to connect to the database.<br /><?php echo $errorMsg ?></li>
-            <?php endif; ?>
-            </ul>
-          </div>
-          <div class="column">
-            <h4>DebugKit</h4>
-            <ul>
-            <?php if (Plugin::isLoaded('DebugKit')) : ?>
-              <li class="bullet success">DebugKit is loaded.</li>
-            <?php else : ?>
-              <li class="bullet problem">DebugKit is NOT loaded. You need to either install pdo_sqlite, or define the "debug_kit" connection name.</li>
-            <?php endif; ?>
-            </ul>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="column links">
-            <h3>Getting Started</h3>
-            <a target="_blank" href="https://book.cakephp.org/4/en/">CakePHP Documentation</a>
-            <a target="_blank" href="https://book.cakephp.org/4/en/tutorials-and-examples/cms/installation.html">The 20 min CMS Tutorial</a>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="column links">
-            <h3>Help and Bug Reports</h3>
-            <a target="_blank" href="irc://irc.freenode.net/cakephp">irc.freenode.net #cakephp</a>
-            <a target="_blank" href="http://cakesf.herokuapp.com/">Slack</a>
-            <a target="_blank" href="https://github.com/cakephp/cakephp/issues">CakePHP Issues</a>
-            <a target="_blank" href="http://discourse.cakephp.org/">CakePHP Forum</a>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="column links">
-            <h3>Docs and Downloads</h3>
-            <a target="_blank" href="https://api.cakephp.org/">CakePHP API</a>
-            <a target="_blank" href="https://bakery.cakephp.org">The Bakery</a>
-            <a target="_blank" href="https://book.cakephp.org/4/en/">CakePHP Documentation</a>
-            <a target="_blank" href="https://plugins.cakephp.org">CakePHP plugins repo</a>
-            <a target="_blank" href="https://github.com/cakephp/">CakePHP Code</a>
-            <a target="_blank" href="https://github.com/FriendsOfCake/awesome-cakephp">CakePHP Awesome List</a>
-            <a target="_blank" href="https://www.cakephp.org">CakePHP</a>
-          </div>
-        </div>
-        <hr>
-        <div class="row">
-          <div class="column links">
-            <h3>Training and Certification</h3>
-            <a target="_blank" href="https://cakefoundation.org/">Cake Software Foundation</a>
-            <a target="_blank" href="https://training.cakephp.org/">CakePHP Training</a>
-          </div>
-        </div>
-      </div>
+    <div style="<?= $this->Html->style([
+        'position' => 'relative',
+        'bottom' => '-40px',
+        'margin' => '36px 0 0 ',
+      ]) ?>">
+      <img src="<?= $this->Url->image('usecase_1.png') ?>" style="<?= $this->Html->style([
+          'display' => 'block',
+          'width' => '100%',
+        ]) ?>">
     </div>
-  </main>
-</body>
+  </body>
 </html>
 
