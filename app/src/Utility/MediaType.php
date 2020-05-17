6c1f00975ae24cd957f70a7a4778c1cab1daee23
WatashiHaNeko <dev@neko.report>
2020-05-17 22:53:33 +0900

    メディアタイプのユーティリティメソッドを外部化

---
+++ app/src/Utility/MediaType.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Utility;
+
+class MediaType {
+  const MEDIA_TYPE_EXT_MAP = [
+    'image/jpeg' => 'jpg',
+    'image/png' => 'png',
+  ];
+
+  public static function checkMediaTypeSupported(string $mediaType): bool {
+    return array_key_exists($mediaType, self::MEDIA_TYPE_EXT_MAP);
+  }
+
+  public static function getExtByMediaType(string $mediaType): ?string {
+    if (!self::checkMediaTypeSupported($mediaType)) {
+      return null;
+    }
+
+    return self::MEDIA_TYPE_EXT_MAP[$mediaType];
+  }
+}
+
